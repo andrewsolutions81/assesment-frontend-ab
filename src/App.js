ae5f290e6fcfb6efffe6e5307ba229ede6cd0a8d
Andres_Berrio <andrewsolutions81@gmail.com>
2022-07-26 14:26:03 -0500

    feat router create menu bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Menu from './components/Menu';
+import Home from './pages/Home';
+import About from './pages/About';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
     <div className="App">
-      <div>hola</div>
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='' element={<About />} />
+          <Route path='' element={<ProductDetail />} />
+        </Routes>
+  </BrowserRouter>
     </div>
   );
 }
