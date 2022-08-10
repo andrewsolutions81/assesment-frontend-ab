1a8c35a2072cc5bfa3e7871175957d2cfbd8a52b
Andres_Berrio <andrewsolutions81@gmail.com>
2022-08-10 12:29:06 -0500

    feat timer button functionality working, missing style

--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import Timer from '../Timer/Timer'
 import {useNavigate} from 'react-router-dom'
 
@@ -7,16 +7,38 @@ function SingleProduct({product}) {
 
   const navigate = useNavigate()
 
-  const handleClick = (id) => {
-    navigate(`../product-detail/${id}`);
-  }
+  const [intro, setIntro] = useState(true);
+	const [buttonStyle, setButtonStyle] = useState(
+		'productCard__detail__buttom button'
+	);
+	const [availabilityText, setAvailabilityText] = useState('Go to detail');
+
+	const hadleIntro = () => {
+		if (intro) {
+			navigate(`../product-detail/${product.id}`, { replace: true });
+		}
+	};
+
+	useEffect(() => {
+		const changeColorButton = () => {
+			if (intro === false) {
+				setButtonStyle('product_button_available');
+				setAvailabilityText('Product unabailable');
+			}
+		};
+		changeColorButton();
+	}, [intro]);
 
   return(
     <li className="product_card" key={product.id}>
       <h2 className="produc_title">{product.title}</h2>
-      <img className="product-image" src={product.image} alt="Cool product, white background"></img>
-      <Timer />
-      <button  type="button" onClick={() => handleClick(product.id)} className="productlist-button">Go to detail</button>
+      <img className="product_image" src={product.image} alt="Cool product, white background"></img>
+      <div className={buttonStyle}>
+        <button onClick={hadleIntro}>{availabilityText}</button>
+        {intro ?
+        <Timer setIntro={setIntro} />
+        : null}
+      </div>
     </li>
   )
 }
